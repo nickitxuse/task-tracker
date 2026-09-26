e81e4b536b27e245a03da94b76121af8df8bd967
rxsemvn <nickitxuse@gmail.com>
2026-09-26 18:05:45 +0600

    feat: added console menu and task creation

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -1,6 +1,6 @@
 package com.practice;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 
 
 public class Main {
@@ -8,15 +8,48 @@ public class Main {
         
         ArrayList<Task> taskList = new ArrayList<>();
 
-        taskList.add(new Task(1, "Закончить настройку GitHub"));
-        taskList.add(new Task(2, "Провести тесты программы"));
-        taskList.add(new Task(3, "Помыться"));
+        Scanner scanner = new Scanner(System.in);
+        int idCounter = 1;
+        boolean running = true;
+        
+        while(running == true){
+            System.out.println("Меню: \n 1 - Список задач.\n 2 - Добавить задачу. \n 0 - Закрыть. \n " );
 
-        taskList.get(0).complete();
+            String command = scanner.nextLine();
 
-        for(Task task : taskList){
-            System.out.println(task);
-        }
+            switch(command){
+                case("2"):
+                    System.out.println("Название задачи: ");
+                    String title = scanner.nextLine();
+                    System.out.println("\n");
+                    taskList.add(new Task(idCounter, title));
+                    System.out.println("Задача добавлена! \n");
+                    idCounter++;
+                    break;
+
+                case("1"):
+                    System.out.println("Список задач: ");
+
+                    if(taskList.isEmpty()){
+                        System.out.println("Список пуст.");
+                    }
+                    else{
+                        for(Task task : taskList)
+                        {System.out.println(task);}
+                        }
+
+                    System.out.println("\n");
+                    break;
+
+                case("0"):
+                    running = false;
+                    break;
+                    
+                default:
+                    System.out.println("Неверная комманда.\n");
+            }       
+
+        }        
 
     }
 }
